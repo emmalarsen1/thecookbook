716b5876d698441ae5da78550979d5b7a432afa8
“Emma <emmalarsen2704@gmail.com>
2024-04-05 14:35:27 +0200

    done

---
+++ modules/settings.js
@@ -0,0 +1,4 @@
+export const apikey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1Y29kbndrcGdxaXpibHltdWN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE2MzEsImV4cCI6MjAyNjMyNzYzMX0.MLNk6YmiGznEuXfhBhKjE8KtPPvGCPoQD4ZSJlV95k8";
+export const endpoint =
+  "https://uucodnwkpgqizblymucy.supabase.co/rest/v1/recipes";
